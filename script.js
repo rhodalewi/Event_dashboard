54161dfad3ff561971161b43514e8ec30571bbf6
Alewi Rhoda Oyinkansola <rhodalewi@gmail.com>
2025-04-03 09:16:55 +0100

    Update

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const full_nav = document.querySelector('.full_navBar');
-const collapse_nav = document.querySelector('.collapse_nav');
-const collapse_icon = document.querySelector('.collapseAll');
+const collapse_icon = document.querySelectorAll('.collapseNav_icon');
 const fullNav_active = document.querySelectorAll('.fullNav_active');
 const homeNav = document.querySelector('.home');
+const navBar = document.querySelector('.navBar');
 
 /* TOGGLE THE FULL NAV AND COLLAPSE NAV */
 function toggleNav() {
-    full_nav.classList.toggle('hidden');
-    collapse_nav.classList.toggle('visible');
-    collapse_icon.classList.toggle('rotate')
+    navBar.classList.toggle('collapsed');
+    collapse_icon.forEach(icon => icon.classList.toggle('rotate'));
 }
 
 /* FULL NAV ACTIVE STATE */
@@ -22,23 +20,39 @@ fullNav_active.forEach((nav, index) => {
 
 /* HOME NAV HOVER STATE */
 homeNav.addEventListener('click', () => {
-    homeNav.classList.toggle('focus');
-    homeNav.classList.add('home_active');
-
-    homeNav.addEventListener('mouseover', () => {
+    if (window.innerWidth <= 768) {
+        homeNav.classList.toggle('focus');
         homeNav.classList.add('home_active');
-    });
-   
-    homeNav.addEventListener('mouseout', () => {
+    
+        homeNav.addEventListener('mouseover', () => {
+            homeNav.classList.add('home_active');
+        });
+       
+        homeNav.addEventListener('mouseout', () => {
+            homeNav.classList.remove('home_active');
+        });
+        
+    } else {
+        homeNav.classList.toggle('focus');
         homeNav.classList.remove('home_active');
-    });
-})
+    
+        homeNav.addEventListener('mouseover', () => {
+            homeNav.classList.remove('home_active');
+        });
+       
+        homeNav.addEventListener('mouseout', () => {
+            homeNav.classList.add('home_active');
+        });
+    }
+   
+});
 
 /* CAROUSAL SLIDE */
 const carousals_img = document.querySelectorAll('.carousal_img');
 const previousBtn = document.querySelector('.carousal_previous');
 const nextBtn = document.querySelector('.carousal_next');
 const carousal_indicator = document.querySelectorAll('.carousal_indicator');
+
 
 let index = 0;
 
@@ -89,15 +103,74 @@ inputs.forEach((input) => {
 /* OPEN MODEL */
 const open_modal = document.querySelector('.open_modal');
 const modal = document.querySelector('.modal');
-const cancel_modal = document.querySelector('.cancel');
 const opacity = document.querySelector('.opacity');
 
 open_modal.addEventListener('click', () => {
     modal.style.display = "flex";
-    opacity.style.display = 'block';
+    opacity.style.display = 'block'; 
 });
 
-cancel_modal.addEventListener('click', () => {
+function cancelButton() {
     modal.style.display = 'none';
     opacity.style.display = 'none';
+}
+
+//MOBILE DUNCTIONALITIES
+/* Open and close the navbar */
+const menuIcon = document.getElementById('menu_icon');
+const cancelIcon = document.querySelector('.cancelIcon');
+const allNav = document.querySelector('.allNav');
+const main_content = document.querySelector('.main_content');
+
+function openNav() {
+    allNav.style.display = 'flex';
+    cancelIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+    main_content.style.display = 'none';
+}
+
+function closeNav() { 
+    allNav.style.display = 'none';
+    cancelIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+    main_content.style.display = 'block';
+}
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth < 768) {
+        openNav();
+        closeNav();
+    }
+})
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 768) {
+        allNav.style.display = "flex";
+    } else {
+        allNav.style.display = "none";
+    }
+});
+
+
+//Data table functionality
+const dataTable = document.querySelectorAll('.accordion_header');
+const dataDropdown = document.querySelectorAll('.dataDropdown');
+const dataIcon = document.querySelectorAll('.dataIcon');
+
+dataTable.forEach((data, index) => {
+    data.addEventListener('click', () => {
+        dataDropdown[index].classList.toggle('dropdownShow');
+        dataIcon[index].classList.toggle('dropArrow');
+    });
+});
+
+
+//TOGGLE DARK MODE
+const body = document.querySelector('body');
+const toggleMode = document.querySelector('.toggleMode');
+
+toggleMode.addEventListener('click', () => {
+    toggleMode.classList.toggle('switch');
+
+    body.classList.toggle('switchMode');
 })
